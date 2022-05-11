915e3f44a7306ed65a4e04181a4206070ca69006
Hayden <asianhaydenxd@gmail.com>
2022-05-10 23:58:35 -0400

    Create getWinner method

--- src/App.java
+++ src/App.java
@@ -5,14 +5,28 @@ enum Piece {
 }
 
 class Game {
-    private Piece[][] board = {
+    Piece[][] board = {
         {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
         {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
         {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
     };
 
-    Piece[][] getBoard() {
-        return this.board;
+    Piece getWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2]) {
+                return this.board[i][0];
+            }
+            if (this.board[0][i] == this.board[1][i] && this.board[0][i] == this.board[2][i]) {
+                return this.board[0][i];
+            }
+        }
+        if (this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2]) {
+            return this.board[0][0];
+        }
+        if (this.board[0][2] == this.board[1][1] && this.board[0][2] == this.board[2][0]) {
+            return this.board[0][2];
+        }
+        return Piece.EMPTY;
     }
 }
 
@@ -20,6 +34,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Game game = new Game();
         
-        System.out.println(Arrays.deepToString(game.getBoard()));
+        System.out.println(Arrays.deepToString(game.board));
     }
 }
