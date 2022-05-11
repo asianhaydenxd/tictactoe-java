7478d3c512676f37edfd4a195bc547bb68c73357
Hayden <asianhaydenxd@gmail.com>
2022-05-10 23:39:31 -0400

    Implement beginning class structures

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
+enum Piece {
+    X, O, EMPTY
+}
+
+class Game {
+    private Piece[][] board = {
+        {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+        {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+        {Piece.EMPTY, Piece.EMPTY, Piece.EMPTY},
+    };
+
+    Piece[][] getBoard() {
+        return this.board;
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Game game = new Game();
+        
+        System.out.println(Arrays.deepToString(game.getBoard()));
     }
 }
