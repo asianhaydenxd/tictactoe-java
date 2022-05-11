6a7eca3c299c1a15fdc190c6a0ab7ec5adccbc70
Hayden <asianhaydenxd@gmail.com>
2022-05-11 00:01:50 -0400

    Alter order of getWinner method

--- src/App.java
+++ src/App.java
@@ -12,6 +12,12 @@ class Game {
     };
 
     Piece getWinner() {
+        if (this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2]) {
+            return this.board[0][0];
+        }
+        if (this.board[0][2] == this.board[1][1] && this.board[0][2] == this.board[2][0]) {
+            return this.board[0][2];
+        }
         for (int i = 0; i < 3; i++) {
             if (this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2]) {
                 return this.board[i][0];
@@ -19,12 +25,6 @@ class Game {
             if (this.board[0][i] == this.board[1][i] && this.board[0][i] == this.board[2][i]) {
                 return this.board[0][i];
             }
-        }
-        if (this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2]) {
-            return this.board[0][0];
-        }
-        if (this.board[0][2] == this.board[1][1] && this.board[0][2] == this.board[2][0]) {
-            return this.board[0][2];
         }
         return Piece.EMPTY;
     }
